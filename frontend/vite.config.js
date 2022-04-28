d251c925f589d7d1b90793b80e85a7b17fe26900
Chayakorn63130500017 <chayakorn.j@mail.kmutt.ac.th>
2022-04-29 04:17:18 +0700

    [+] first production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        // target: 'http://10.4.56.127:8080',
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+    plugins: [vue()],
+    server: {
+        proxy: {
+            "/api": {
+                // target: "http://10.4.56.127:8080",
+                target: `http://oasipbackend:8080`,
+                changeOrigin: true,
+                secure: false,
+                // rewrite: (path) => path.replace(/^\/api/, '')
+            },
+        },
+    },
+});
