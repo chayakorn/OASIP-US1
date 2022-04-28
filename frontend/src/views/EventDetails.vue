95323d4856310b84b4ca83d4b366568d85e5db7b
Kanjanaporn63130500005 <yokkanjanaporn@gmail.com>
2022-04-29 00:05:45 +0700

    tst con

--- frontend/src/views/EventDetails.vue
+++ frontend/src/views/EventDetails.vue
@@ -30,7 +30,7 @@ console.log(time)
             <div class="font-bold text-xl text-center">Scheduled details</div>
             <div>
               <div class=" float-right ">
-                <button class="hover:bg-[#F1F3F4] transition duration-100  rounded-lg grid " @click="closeModal">
+                <button class="hover:bg-[#F1F3F4] transition duration-150  rounded-lg grid " @click="closeModal">
                   <svg width="2.5em" height="2.5em" viewBox="0 0 24 24" class="">
                     <path fill="currentColor"
                       d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z">
