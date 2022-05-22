a3670d55908b76a965f52165cf48cbbd18695c8c
Chayakorn63130500017 <chayakorn.j@mail.kmutt.ac.th>
2022-05-23 05:01:50 +0700

    [fixed] path eventcatagory

--- backend/src/main/java/oasip/us1/backend/controller/EventcategoryController.java
+++ backend/src/main/java/oasip/us1/backend/controller/EventcategoryController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/eventcat")
+@RequestMapping("/api/event-catagories")
 @CrossOrigin
 public class EventcategoryController {
     @Autowired
