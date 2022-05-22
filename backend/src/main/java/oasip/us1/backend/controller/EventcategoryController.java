fa37929f3a49ea7ccef21856eac377b7b3833e20
Chayakorn63130500017 <chayakorn.j@mail.kmutt.ac.th>
2022-05-23 05:04:16 +0700

    [+] categor

--- backend/src/main/java/oasip/us1/backend/controller/EventcategoryController.java
+++ backend/src/main/java/oasip/us1/backend/controller/EventcategoryController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/event-catagories")
+@RequestMapping("/api/event-categories")
 @CrossOrigin
 public class EventcategoryController {
     @Autowired
