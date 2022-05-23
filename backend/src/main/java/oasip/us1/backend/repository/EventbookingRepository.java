f28089ce155fcd676997ff969a13a2906ea5601c
Chayakorn63130500017 <chayakorn.j@mail.kmutt.ac.th>
2022-05-24 01:58:42 +0700

    [-]anotation

--- backend/src/main/java/oasip/us1/backend/repository/EventbookingRepository.java
+++ backend/src/main/java/oasip/us1/backend/repository/EventbookingRepository.java
@@ -13,23 +13,21 @@ import java.util.Collection;
 import java.util.List;
 
 public interface EventbookingRepository extends JpaRepository<Eventbooking, Integer> {
-    @Modifying
-    @Transactional
+
     @Query(
             value = "select * from eventbooking where ((:start between addtime(eventStartTime,'00:01:00') and addtime(eventStartTime,concat('00:',eventDuration-1,':00'))) or ( :end between addtime(eventStartTime,'00:01:00') and addtime(eventStartTime,concat('00:',eventDuration-1,':00')) )or (addtime(eventStartTime,'00:01:00') between :start and :end) or (addtime(eventStartTime,concat('00:',eventDuration-1,':00')) between :start and :end))  and eventCategoryId = :catid",
             nativeQuery = true
     )
     Collection<Eventbooking> findByEventStartTimeBetween(@Param("start") String start,@Param("end") String end,@Param("catid") int catid);
 
-    @Modifying
-    @Transactional
+
     @Query(
             value = "select * from eventbooking where ((:start between addtime(eventStartTime,'00:01:00') and addtime(eventStartTime,concat('00:',eventDuration-1,':00'))) or ( :end between addtime(eventStartTime,'00:01:00') and addtime(eventStartTime,concat('00:',eventDuration-1,':00')) )or (addtime(eventStartTime,'00:01:00') between :start and :end) or (addtime(eventStartTime,concat('00:',eventDuration-1,':00')) between :start and :end))  and eventCategoryId = :catid and bookingId != :bid",
             nativeQuery = true
     )
     Collection<Eventbooking> findByEventStartTimeBetweenForPut(@Param("start") String start,@Param("end") String end,@Param("catid") int catid,@Param("bid") int bid);
 
-    @Transactional
+
     @Query(
             value = "select * from eventbooking where eventCategoryId in :id",
             countQuery = "select count(*) from eventbooking where eventCategoryId in :id",
